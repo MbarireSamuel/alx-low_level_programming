710012005482316ab176e8369729a99f16f77611
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-09 14:46:37 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	s = 'z';
+	while
+		(s >= 'a') {
+			putchar(s);
+			s--;
+		}
+	putchar('\n');
+	return (0);
+}
