816b461bd21e16e23ca0c50e43044d385c2caa8a
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-09 15:04:04 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	int m;
+
+	s = 'a';
+	m = 0;
+	while
+		(m < 10) {
+			putchar(m + 'a');
+			m++;
+		}
+	while
+		(s <= 'f') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
