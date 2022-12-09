2cbe738e4505a87459d1cebd24123bd22d3e95f5
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-09 06:48:15 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char s;
+
+	char m;
+
+	s = 'a';
+	m = 'A';
+	while
+		(s <= 'z') {
+			putchar(s);
+			s++;
+		}
+	while
+		(m <= 'Z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
