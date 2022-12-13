3ef364828187422f64c458f477421a12ad0cc4b2
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-13 14:17:05 -0800

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int s);
+int _isalpha(int s);
+int print_sign(int m);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int m);
+void print_times_table(int m);
+void num(int r,int m);
+
+#endif
