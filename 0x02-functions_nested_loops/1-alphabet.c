685083a6deede20c675ddbfed16baea0f7e0964b
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-13 14:45:47 -0800

    alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * 
+ * Return: 0
+ */
+
+void print_alphabet(void)
+
+{
+	char s = 'a';
+
+	while (s <= 'z')
+	{
+		_putchar(s);
+		s++;
+	}
+
+	_putchar('\n');
+
+}
