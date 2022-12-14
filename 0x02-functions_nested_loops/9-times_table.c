ff8d906be5172e09dd4a9101bceea61bf94fa25a
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-14 12:34:35 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /***
- * times_table -print the 9 times table
+ * times_table - print the 9 times table
  */
 void times_table(void)
 {
@@ -18,7 +18,7 @@ void times_table(void)
 
 			prod = num * MULT;
 
-			if ( prod <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
