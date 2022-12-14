fa6e272a589aa99dde21ebad0a2a096ff854caff
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-14 14:12:21 -0800

    starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,17 +2,20 @@
 
 /**
  * jack_bauer - prints every minute of the day
+ * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
 
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
+	hour = 0;
 
-		{
+	while (hour < 24)
+	{
+			minute = 0;
+			while (minute < 60)
+			{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
 			_putchar(':');
@@ -21,6 +24,8 @@ void jack_bauer(void)
 			_putchar((minute % 10) + '0');
 			
 			_putchar('\n');
+			minute++;
 		}
-	}	
+		hour++;
+	}
 }
