65440eb083787d0ff2ff953c0c640e6e2e535f2a
MbarireSamuel <mbariresamuel98@gmail.com>
2022-12-14 14:39:57 -0800

    times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
-int main(void)
+void print_times_table(int n)
 {
 	print_times_table(3);
 	 _putchar('\n');
